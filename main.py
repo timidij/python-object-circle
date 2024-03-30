15884e75450e54d4c5169050f0170c97d3f8b75f
Odubo Timidi James <81032342+timidij@users.noreply.github.com>
2024-03-30 19:50:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-class Circle():
+
+class Circle:
     pi = 3.142
-    def __init__ (self, radius):
+    def __init__(self, radius):
         self.radius = radius
 
     def circumference(self):
-        return self.radius * self.pi
-cool = Circle( 4 )
-print(cool.circumference())
+        return self.radius*self.pi
+
+    def area(self):
+        return self.pi*(self.radius**2)
+circle1 = Circle(4)
+print(f"the circumference of the circle is {circle1.circumference()}")
+print(f"the area of the circle is {circle1.area()}")
